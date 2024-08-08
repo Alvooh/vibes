6f7017fad4e58929aa4ceb54ab42ccbb7edf12fc
AlvinObala <alvinomolo99@gmail.com>
2024-08-08 23:50:50 +0300

    cook doc

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Question from "./components/Question";
+
+function App() {
+  return (
+    <div>
+      <Question />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Question.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./Question.css";
+
+const Question = () => {
+  const [position, setPosition] = useState({ top: "0px", left: "0px" });
+
+  useEffect(() => {
+    const moveButton = () => {
+      const containerWidth = window.innerWidth - 100; // Adjust to the actual button size
+      const containerHeight = window.innerHeight - 50; // Adjust to the actual button size
+
+      const newTop = Math.random() * containerHeight + "px";
+      const newLeft = Math.random() * containerWidth + "px";
+      setPosition({ top: newTop, left: newLeft });
+    };
+
+    const intervalId = setInterval(() => {
+      moveButton();
+    }, 300); 
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, []);
+
+  const handleYesClick = () => {
+    alert("You clicked Yes!");
+  };
+
+  return (
+    <div className="container">
+      <h1 className="question">Are you gay Doc?</h1>
+      <div className="buttons">
+        <button className="yesButton" onClick={handleYesClick}>
+          Yes
+        </button>
+        <button
+          className="noButton"
+          style={{ top: position.top, left: position.left }}
+        >
+          No
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
